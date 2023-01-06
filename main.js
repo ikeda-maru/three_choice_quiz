0d035d6aa8bb581878ef9f4a03e6e835ba69e360
atsushi-maru <ikeda.atsushi00@gmail.com>
2023-01-06 20:31:37 +0900

    set behavior on click

--- main.js
+++ main.js
@@ -11,10 +11,31 @@ function render(quiz) {
   const ul = document.createElement('ul');
   const li0 = document.createElement('li');
   li0.textContent = quiz[1];
+  li0.addEventListener('click', () => {
+    if (quiz[4] === 0) {
+      li0.classList.add('correct');
+    } else {
+      li0.classList.add('wrong');
+    }
+  })
   const li1 = document.createElement('li');
   li1.textContent = quiz[2];
+  li1.addEventListener('click', () => {
+    if (quiz[4] === 1) {
+      li1.classList.add('correct');
+    } else {
+      li1.classList.add('wrong');
+    }
+  })
   const li2 = document.createElement('li');
   li2.textContent = quiz[3];
+  li2.addEventListener('click', () => {
+    if (quiz[4] === 2) {
+      li2.classList.add('correct');
+    } else {
+      li2.classList.add('wrong');
+    }
+  })
 
   ul.appendChild(li0);
   ul.appendChild(li1);
